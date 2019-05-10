ef9f2eb2ba2601aeada5aa68f2dd9da47a72ad45
aldianfn <aldian.1998@gmail.com>
2019-05-10 13:15:35 +0700

    Update 4

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/kereta_join/';
+$config['base_url'] = 'http://localhost/PR7_1700635_AldianFN/';
 
 /*
 |--------------------------------------------------------------------------
